f7b67340cacdccd8670e9c24c8b6a8f3aad71a1b
IsaDevs <isabel.calzadilla.18@gmail.com>
2023-05-03 17:15:52 +0200

    commit 4 : start the binding on mainACTIVITY --> ok

--- app/src/main/java/com/example/dragonball_api_kc_android/MainActivity.kt
+++ app/src/main/java/com/example/dragonball_api_kc_android/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.dragonball_api_kc_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dragonball_api_kc_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    /**
+     *  PROPIEDAD 'BINDING' PARA ENLAZAR AL LAYOUT
+     */
+    private lateinit var  binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // DEFINICIÓN DE LAS PROPIEDADES DEL LAYOUT AL BINDING
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        // INDICACIÓN DEL CONTENTVIEW CON EL VALOR DEL BINDING
+        setContentView(binding.root)
     }
 }
